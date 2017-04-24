90f5f07b031d9233066f904ea4bf8ab94f9a09ee
jazzmahima <keziajazzz@gmail.com>
2017-04-24 10:59:56 +0530

    Update and rename vj.c to jjj.c

---
+++ jjj.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a=16;
+  if(a>0)
+  {
+    printf("positive");
+  }
+  else if(a<0)
+  {
+    printf("negative");
+  }
+  else
+  {
+    printf("zero");
+  }
+  getch();
+}
